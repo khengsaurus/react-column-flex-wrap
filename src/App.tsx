c401f85d9831c881c647d26ff566455d5640326a
tzikheng <tzikheng@gmail.com>
2022-03-01 00:46:25 +0800

    first commit - example created

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [boxes, setBoxes] = useState(7);
+  const [effectOn, setEffectOn] = useState(false);
+  const arr = Array.from(Array(Math.max(0, boxes)));
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* Top boxes showcasing column-wrap behavior */}
+      <div className="row">
+        {/* First column of (grey) boxes */}
+        <div className="column" style={{ zIndex: 10 }}>
+          {arr.map((_, index) => (
+            <div className="big-box grey" key={`big-box-grey-${index}`} />
+          ))}
+        </div>
+        {/* Second column of (purple) boxes */}
+        <div className="column" style={{ zIndex: 1 }}>
+          {arr.map((_, index) => (
+            <div className="big-box purple" key={`big-box-purple-${index}`} />
+          ))}
+        </div>
+      </div>
+
+      {/* Bottom boxes showcasing default row-wrap(reverse) behavior */}
+      <div className="column">
+        {/* First row of (grey) boxes */}
+        <div className="row" style={{ zIndex: 10 }}>
+          {arr.map((_, index) => (
+            <div className="small-box grey" key={`small-box-grey-${index}`} />
+          ))}
+        </div>
+        {/* Second row of (purple) boxes */}
+        <div className="row" style={{ zIndex: 1 }}>
+          {arr.map((_, index) => (
+            <div
+              className="small-box purple"
+              key={`small-box-purple-${index}`}
+            />
+          ))}
+        </div>
+      </div>
+
+      {/* Legend */}
+      <div className="column legend">
+        <p>{effectOn ? "Behavior: modified" : "Behavior: default"}</p>
+        <p>{`Grey boxes: ${boxes}`}</p>
+        <p>{`Purple boxes: ${boxes}`}</p>
+        <div className="row">
+          <button onClick={() => setBoxes(boxes - 1)} disabled={boxes === 0}>
+            -
+          </button>
+          <button onClick={() => setBoxes(boxes + 1)}>+</button>
+          <button className="big-button" onClick={() => setEffectOn(!effectOn)}>
+            Toggle effect
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
