00fe50da71dff1ca5317be55afb1f202677e770d
tzikheng <tzikheng@gmail.com>
2022-03-10 23:32:02 +0800

    edit test

--- tests/components/Column.test.tsx
+++ tests/components/Column.test.tsx
@@ -30,58 +30,58 @@ function renderTestColumn(
 }
 
 describe("Renders Column with children and correct dimensions", () => {
-  // it("no styles, defaults to column wrap", () => {
-  //   const { getByTestId } = render(
-  //     <Column id="test-column">
-  //       {[...Array(1).keys()].map((i) => renderTestBox(i, 20))}
-  //     </Column>
-  //   );
+  it("no styles, defaults to column wrap", () => {
+    const { getByTestId } = render(
+      <Column id="test-column">
+        {[...Array(1).keys()].map((i) => renderTestBox(i, 20))}
+      </Column>
+    );
 
-  //   const testColumn = getByTestId("rcfw-c");
-  //   expect(testColumn).toHaveStyle("display: flex");
-  //   expect(testColumn).toHaveStyle("flex-direction: column");
-  //   expect(testColumn).toHaveStyle("flex-wrap: wrap");
-  // });
+    const testColumn = getByTestId("rcfw-c");
+    expect(testColumn).toHaveStyle("display: flex");
+    expect(testColumn).toHaveStyle("flex-direction: column");
+    expect(testColumn).toHaveStyle("flex-wrap: wrap");
+  });
 
-  // it("column wrap, max-height via in-line style, 5 children of dimensions increasing from 20px by 1px", () => {
-  //   const { getByTestId } = render(
-  //     <Column id="test-column" style={{ maxHeight: 50 }}>
-  //       {[...Array(5).keys()].map((i) => renderTestBox(i, 20))}
-  //     </Column>
-  //   );
+  it("column wrap, max-height via in-line style, 5 children of dimensions increasing from 20px by 1px", () => {
+    const { getByTestId } = render(
+      <Column id="test-column" style={{ maxHeight: 50 }}>
+        {[...Array(5).keys()].map((i) => renderTestBox(i, 20))}
+      </Column>
+    );
 
-  //   const testColumn = getByTestId("rcfw-c");
-  //   for (let i = 0; i < 5; i++) {
-  //     const box = getByTestId("box-" + i);
-  //     expect(box).toBeTruthy();
-  //     expect(box).toHaveStyle(`width: ${20 + i}px`);
-  //   }
-  //   expect(testColumn).toHaveStyle("display: flex");
-  //   expect(testColumn).toHaveStyle("flex-direction: column");
-  //   expect(testColumn).toHaveStyle("flex-wrap: wrap");
-  //   expect(testColumn).toHaveStyle("max-height: 50px");
-  //   expect(testColumn).toHaveStyle("width: 68px"); // 21px + 23px + 24px
-  // });
+    const testColumn = getByTestId("rcfw-c");
+    for (let i = 0; i < 5; i++) {
+      const box = getByTestId("box-" + i);
+      expect(box).toBeTruthy();
+      expect(box).toHaveStyle(`width: ${20 + i}px`);
+    }
+    expect(testColumn).toHaveStyle("display: flex");
+    expect(testColumn).toHaveStyle("flex-direction: column");
+    expect(testColumn).toHaveStyle("flex-wrap: wrap");
+    expect(testColumn).toHaveStyle("max-height: 50px");
+    expect(testColumn).toHaveStyle("width: 68px"); // 21px + 23px + 24px
+  });
 
-  // it("column wrap via css, 5 children of dimensions increasing from 20px by 1px", () => {
-  //   const { getByTestId } = render(
-  //     <Column className={"column-reverse-wrap-reverse _50px"} id="test-column">
-  //       {[...Array(5).keys()].map((i) => renderTestBox(i, 20))}
-  //     </Column>
-  //   );
+  it("column wrap via css, 5 children of dimensions increasing from 20px by 1px", () => {
+    const { getByTestId } = render(
+      <Column className={"column-reverse-wrap-reverse _50px"} id="test-column">
+        {[...Array(5).keys()].map((i) => renderTestBox(i, 20))}
+      </Column>
+    );
 
-  //   const testColumn = getByTestId("rcfw-c");
-  //   for (let i = 0; i < 5; i++) {
-  //     const box = getByTestId("box-" + i);
-  //     expect(box).toBeTruthy();
-  //     expect(box).toHaveStyle(`width: ${20 + i}px`);
-  //   }
-  //   expect(testColumn).toHaveStyle("display: flex");
-  //   expect(testColumn).toHaveStyle("flex-direction: column-reverse");
-  //   expect(testColumn).toHaveStyle("flex-wrap: wrap-reverse");
-  //   expect(testColumn).toHaveStyle("max-height: 50px");
-  //   expect(testColumn).toHaveStyle("width: 68px"); // 21px + 23px + 24px
-  // });
+    const testColumn = getByTestId("rcfw-c");
+    for (let i = 0; i < 5; i++) {
+      const box = getByTestId("box-" + i);
+      expect(box).toBeTruthy();
+      expect(box).toHaveStyle(`width: ${20 + i}px`);
+    }
+    expect(testColumn).toHaveStyle("display: flex");
+    expect(testColumn).toHaveStyle("flex-direction: column-reverse");
+    expect(testColumn).toHaveStyle("flex-wrap: wrap-reverse");
+    expect(testColumn).toHaveStyle("max-height: 50px");
+    expect(testColumn).toHaveStyle("width: 68px"); // 21px + 23px + 24px
+  });
 
   it("column-reverse wrap-reverse via props, 5 children of dimensions increasing from 20px by 1px", () => {
     const { getByTestId } = render(
